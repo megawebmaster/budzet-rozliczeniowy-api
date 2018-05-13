20d7566207ac56d1bbb7d5573eeb9033f5f89d10
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2018-05-13 15:09:05 +0200

    Add missing migration

---
+++ src/Migrations/Version20180513094553.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180513094553 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE budget_entry DROP FOREIGN KEY FK_4030088612469DE2');
+        $this->addSql('ALTER TABLE budget_entry ADD CONSTRAINT FK_4030088612469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE budget_expense DROP FOREIGN KEY FK_EFB9C74712469DE2');
+        $this->addSql('ALTER TABLE budget_expense ADD CONSTRAINT FK_EFB9C74712469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE budget_entry DROP FOREIGN KEY FK_4030088612469DE2');
+        $this->addSql('ALTER TABLE budget_entry ADD CONSTRAINT FK_4030088612469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE budget_expense DROP FOREIGN KEY FK_EFB9C74712469DE2');
+        $this->addSql('ALTER TABLE budget_expense ADD CONSTRAINT FK_EFB9C74712469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+    }
+}
