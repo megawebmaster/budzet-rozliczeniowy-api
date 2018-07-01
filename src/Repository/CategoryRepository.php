b2050f6fa324775be7fa377628c2e051a7f3b98a
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2018-07-01 17:09:49 +0200

    Properly return all months values (including zeros) for averages

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -51,31 +51,44 @@ class CategoryRepository extends ServiceEntityRepository
       $date = $now->sub(new \DateInterval('P1Y'))->format('Y-m').'-01';
 
       $averagesQuery = <<<SQL
-SELECT d.category_id, d.real_value
+SELECT d.category_id, d.real_value, CONCAT(db.year,'-',d.month,'-01') AS entry_id
 FROM budget_entry d
 LEFT JOIN budget_year db ON db.id = d.budget_year_id
-WHERE db.budget_id = '$budgetId' 
-  AND d.real_value != '' 
-  AND STR_TO_DATE(CONCAT(db.year,'-',d.month,'-01'), '%Y-%m-%d') > '$date'
+WHERE db.budget_id = '$budgetId' AND STR_TO_DATE(CONCAT(db.year,'-',d.month,'-01'), '%Y-%m-%d') > '$date'
 SQL;
 
       $mapping = new ResultSetMapping();
       $mapping->addScalarResult('real_value', 'real');
       $mapping->addScalarResult('category_id', 'category_id');
+      $mapping->addScalarResult('entry_id', 'entry_id');
 
       $results = $this->getEntityManager()
         ->createNativeQuery($averagesQuery, $mapping)
         ->getResult(AbstractQuery::HYDRATE_ARRAY);
 
       $averages = [];
+      $firstEntries = [];
       foreach($results as $result)
       {
         if(!isset($averages[$result['category_id']]))
         {
           $averages[$result['category_id']] = [];
         }
+        if(!isset($firstEntries[$result['category_id']]) || $firstEntries[$result['category_id']] > $result['entry_id'])
+        {
+          $firstEntries[$result['category_id']] = $result['entry_id'];
+        }
 
         $averages[$result['category_id']][] = $result['real'];
+      }
+
+      // Fill in missing entries to properly calculate values
+      foreach($firstEntries as $categoryId => $entryId)
+      {
+        $difference = $now->diff(new \DateTime($entryId));
+        if(count($averages[$categoryId]) < $difference->m) {
+          $averages[$categoryId] += array_fill(0, $difference->m - count($averages[$categoryId]), '');
+        }
       }
 
       return $averages;
